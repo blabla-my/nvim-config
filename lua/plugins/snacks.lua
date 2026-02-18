f938fb231e839c05fdf6be30a93a9fb0789835bf
MingyuanLuo <myluo24@m.fudan.edu.cn>
2026-02-18 10:27:53 +0800

    Limit snacks.nvim to grep

--- lua/plugins/snacks.lua
+++ lua/plugins/snacks.lua
@@ -1,34 +1,18 @@
 return {
   {
     "folke/snacks.nvim",
-    priority = 1000,
     keys = {
-      {
-        "<leader><space>",
-        function()
-          require("snacks").picker()
-        end,
-        desc = "Pickers",
-      },
-      {
-        "<leader>ff",
-        function()
-          require("snacks").picker.files()
-        end,
-        desc = "Find Files",
-      },
       {
         "<leader>/",
         function()
           require("snacks").picker.grep()
         end,
-        desc = "Grep",
+        desc = "Global Grep",
       },
     },
     opts = {
       picker = {
         enabled = true,
-        ui_select = false,
       },
     },
   },
